6fff19f494390f3da3d40e77684888ed13461ad9
SHUBHAM7306 <shubhamkumar851130@gmail.com>
2023-01-27 21:02:33 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,30 @@
+let expression = "";
+
+function insert(num){
+  expression += num;
+  const textview = document.querySelector(".textview");
+  textview.value = expression;
+}
+
+function equals(){
+  const textview = document.querySelector(".textview");
+  try{
+    textview.value = eval(expression);
+  }
+  catch(err){
+    textview.value = "Syntax error";
+  }
+  expression = "";
+}
+
+function clean(){
+  expression = "";
+  const textview = document.querySelector(".textview");
+  textview.value = expression;
+}
+
+function back(){
+  expression = expression.slice(0, -1);
+  const textview = document.querySelector(".textview");
+  textview.value = expression;
+}
